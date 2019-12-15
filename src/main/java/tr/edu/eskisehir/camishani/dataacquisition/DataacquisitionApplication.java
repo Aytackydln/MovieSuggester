51f940138d942a1ffee78c02c2a7c55b7ad4d911
Aytackydln <aytackayadelen@gmail.com>
2019-12-15 16:54:11 +0300

    Initial commit

---
+++ src/main/java/tr/edu/eskisehir/camishani/dataacquisition/DataacquisitionApplication.java
@@ -0,0 +1,13 @@
+package tr.edu.eskisehir.camishani.dataacquisition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataacquisitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataacquisitionApplication.class, args);
+	}
+
+}
