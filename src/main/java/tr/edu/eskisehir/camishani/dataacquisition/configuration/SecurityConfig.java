623ab75567ed8f0b703fd199b9e19a525b7e7a24
Aytackydln <aytackayadelen@gmail.com>
2019-12-20 00:51:26 +0300

    Basic security configuration

---
+++ src/main/java/tr/edu/eskisehir/camishani/dataacquisition/configuration/SecurityConfig.java
@@ -0,0 +1,56 @@
+package tr.edu.eskisehir.camishani.dataacquisition.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {  // Spring Security Configuration
+        http
+                .formLogin().defaultSuccessUrl("/index.html");
+        http
+                .csrf().disable();
+        http
+                .logout()
+                .logoutSuccessUrl("/login");
+
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/css/**")
+                .antMatchers("/icon-fonts/**")
+                .antMatchers("/images/**")
+                .antMatchers("/js/**")
+                .antMatchers("index.html");
+    }
+
+    @Autowired
+    public void configAuthentication(final AuthenticationManagerBuilder auth, final DataSource dataSource) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select username, password, enabled from logins where username=?")
+                .authoritiesByUsernameQuery("select username, authority from authorities where username=?");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
