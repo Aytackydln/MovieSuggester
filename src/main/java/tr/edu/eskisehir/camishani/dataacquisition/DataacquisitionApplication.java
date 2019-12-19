74f640a8da2168e003fbfb0244a66001a80bb0bd
Aytackydln <aytackayadelen@gmail.com>
2019-12-20 00:54:10 +0300

    Added server initializer

--- src/main/java/tr/edu/eskisehir/camishani/dataacquisition/DataacquisitionApplication.java
+++ src/main/java/tr/edu/eskisehir/camishani/dataacquisition/DataacquisitionApplication.java
@@ -2,9 +2,10 @@ package tr.edu.eskisehir.camishani.dataacquisition;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DataacquisitionApplication {
+public class DataacquisitionApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataacquisitionApplication.class, args);
